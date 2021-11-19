630d9c4ffd4fa8a3cbe3cd744ac97ebb5a997556
Vladislav <vladkarmanov772@gmail.com>
2021-11-19 18:11:36 +0500

    Commit

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main():
     result = parser.get_sentences()
     dT = pd.DataFrame({'tajik': list(result[0])})
     dP = pd.DataFrame({'farsi': list(result[1])})
-    dT.to_csv('farsi.csv', index=False)
-    dP.to_csv('tajik.csv', index=False)
-    print(pd.read_csv('farsi.csv'), pd.read_csv('tajik.csv'))
+    dT.to_csv('tajik.csv', index=False)
+    dP.to_csv('farsi.csv', index=False)
+    print(pd.read_csv('tajik.csv'), pd.read_csv('farsi.csv'))
 
 
 if __name__ == "__main__":
